b53f7e759a22817c9e20bd27eb904112d4aaa661
Umut Yazgan <umut.yaz@gmail.com>
2018-05-07 21:22:27 +0000

    Initial commit

---
+++ face_landmark_detection.py
@@ -0,0 +1,179 @@
+# #!/usr/bin/python
+# The contents of this file are in the public domain. See LICENSE_FOR_EXAMPLE_PROGRAMS.txt
+#
+#   This example program shows how to find frontal human faces in an image and
+#   estimate their pose.  The pose takes the form of 68 landmarks.  These are
+#   points on the face such as the corners of the mouth, along the eyebrows, on
+#   the eyes, and so forth.
+#
+#   The face detector we use is made using the classic Histogram of Oriented
+#   Gradients (HOG) feature combined with a linear classifier, an image pyramid,
+#   and sliding window detection scheme.  The pose estimator was created by
+#   using dlib's implementation of the paper:
+#      One Millisecond Face Alignment with an Ensemble of Regression Trees by
+#      Vahid Kazemi and Josephine Sullivan, CVPR 2014
+#   and was trained on the iBUG 300-W face landmark dataset (see
+#   https://ibug.doc.ic.ac.uk/resources/facial-point-annotations/):  
+#      C. Sagonas, E. Antonakos, G, Tzimiropoulos, S. Zafeiriou, M. Pantic. 
+#      300 faces In-the-wild challenge: Database and results. 
+#      Image and Vision Computing (IMAVIS), Special Issue on Facial Landmark Localisation "In-The-Wild". 2016.
+#   You can get the trained model file from:
+#   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2.
+#   Note that the license for the iBUG 300-W dataset excludes commercial use.
+#   So you should contact Imperial College London to find out if it's OK for
+#   you to use this model file in a commercial product.
+#
+#
+#   Also, note that you can train your own models using dlib's machine learning
+#   tools. See train_shape_predictor.py to see an example.
+#
+#
+# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
+#   You can install dlib using the command:
+#       pip install dlib
+#
+#   Alternatively, if you want to compile dlib yourself then go into the dlib
+#   root folder and run:
+#       python setup.py install
+#   or
+#       python setup.py install --yes USE_AVX_INSTRUCTIONS
+#   if you have a CPU that supports AVX instructions, since this makes some
+#   things run faster.  
+#
+#   Compiling dlib should work on any operating system so long as you have
+#   CMake installed.  On Ubuntu, this can be done easily by running the
+#   command:
+#       sudo apt-get install cmake
+#
+#   Also note that this example requires scikit-image which can be installed
+#   via the command:
+#       pip install scikit-image
+#   Or downloaded from http://scikit-image.org/download.html. 
+
+import sys
+import os
+import dlib
+import glob
+from skimage import io
+import numpy as np
+import cv2
+
+if len(sys.argv) != 3:
+    print(
+        "Give the path to the trained shape predictor model as the first "
+        "argument and then the directory containing the facial images.\n"
+        "For example, if you are in the python_examples folder then "
+        "execute this program by running:\n"
+        "    ./face_landmark_detection.py shape_predictor_68_face_landmarks.dat ../examples/faces\n"
+        "You can download a trained facial shape predictor from:\n"
+        "    http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2")
+    exit()
+
+predictor_path = sys.argv[1]
+faces_folder_path = sys.argv[2]
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+win = dlib.image_window()
+
+for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
+    print("Processing file: {}".format(f))
+    img = io.imread(f)
+
+    win.clear_overlay()
+    win.set_image(img)
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    dets = detector(img, 1)
+    print("Number of faces detected: {}".format(len(dets)))
+    for k, d in enumerate(dets):
+        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+            k, d.left(), d.top(), d.right(), d.bottom()))
+        # Get the landmarks/parts for the face in box d.
+        shape = predictor(img, d)
+        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
+                                                  shape.part(1)))
+
+        # coordinates of necessary landmarks
+        print("left eye: {}".format(shape.part(36)))
+        print("right eye: {}".format(shape.part(45)))
+        print("tip of nose: {}".format(shape.part(33)))
+        print("left side of lip: {}".format(shape.part(48)))
+        print("right side of lip: {}".format(shape.part(54)))
+        print("chin: {}".format(shape.part(8)))
+
+        # NOTE: HEAD POSE ESTIMATION START
+
+        # Read Image
+        im = cv2.imread(f);
+        size = im.shape
+             
+        #2D image points. If you change the image, you need to change vector
+        image_points = np.array([
+                                    (shape.part(30).x,shape.part(30).y),     # Nose tip
+                                    (shape.part(8).x,shape.part(8).y),     # Chin
+                                    (shape.part(36).x,shape.part(36).y),     # Left eye left corner
+                                    (shape.part(45).x,shape.part(45).y),     # Right eye right corne
+                                    (shape.part(48).x,shape.part(48).y),     # Left Mouth corner
+                                    (shape.part(54).x,shape.part(54).y)      # Right mouth corner
+                                ], dtype="double")
+         
+        # 3D model points.
+        model_points = np.array([
+                                    (0.0, 0.0, 0.0),             # Nose tip
+                                    (0.0, -330.0, -65.0),        # Chin
+                                    (-225.0, 170.0, -135.0),     # Left eye left corner
+                                    (225.0, 170.0, -135.0),      # Right eye right corne
+                                    (-150.0, -150.0, -125.0),    # Left Mouth corner
+                                    (150.0, -150.0, -125.0)      # Right mouth corner
+                                 
+                                ])
+         
+         
+        # Camera internals
+         
+        focal_length = size[1]
+        center = (size[1]/2, size[0]/2)
+        camera_matrix = np.array(
+                                 [[focal_length, 0, center[0]],
+                                 [0, focal_length, center[1]],
+                                 [0, 0, 1]], dtype = "double"
+                                 )
+         
+        print("Camera Matrix :\n {0}".format(camera_matrix))
+         
+        dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
+         
+        print("Rotation Vector:\n {0}".format(rotation_vector))
+        print("Translation Vector:\n {0}".format(translation_vector))
+         
+         
+        # Project a 3D point (0, 0, 1000.0) onto the image plane.
+        # We use this to draw a line sticking out of the nose
+         
+         
+        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+         
+        for p in image_points:
+            cv2.circle(im, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
+         
+         
+        p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+        p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+         
+        cv2.line(im, p1, p2, (255,0,0), 2)
+         
+        # Display image
+        cv2.imshow("Output", im)
+        cv2.waitKey(0)
+
+        # NOTE: HEAD POSE ESTIMATION END
+
+        # Draw the face landmarks on the screen.
+        win.add_overlay(shape)
+
+    win.add_overlay(dets)
+    dlib.hit_enter_to_continue()
